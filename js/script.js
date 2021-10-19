80a686ce25baf80755cef4d01ee207acb61170d9
Adil <Adil523@mail.ru>
2021-10-19 23:14:28 +0600

    y first lesson

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 "use strict";
 
-const newText = document.querySelector(".new_element");
-const newElement = document.createElement("div");
-newElement.innerHTML = "I must, I gonna to work";
+const numberOfFilms = +prompt("Сколько фильмов просмотрели?", "");
 
-newText.after(newElement);
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+	b = prompt("На сколько вы его оцените?", ""),
+	c = prompt("Один из последних просмотренных фильмов?", ""),
+	d = prompt("На сколько вы его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
